debc78654f55b4058566640cdb71d36044c0aa22
Shawn Lee <amarese@naver.com>
2019-04-02 21:15:06 +0900

    Updates

---
+++ precache-manifest.f7d52462f378c19e55e79a76fbf51130.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "68ccfe6b35537e4ae71b",
+    "url": "/movie_app/static/js/main.46017222.chunk.js"
+  },
+  {
+    "revision": "74286a96ace76faac2ea",
+    "url": "/movie_app/static/js/2.5425dfe7.chunk.js"
+  },
+  {
+    "revision": "68ccfe6b35537e4ae71b",
+    "url": "/movie_app/static/css/main.40922b6d.chunk.css"
+  },
+  {
+    "revision": "5f031788744e6ed77da61497bef5f1a5",
+    "url": "/movie_app/index.html"
+  }
+];
